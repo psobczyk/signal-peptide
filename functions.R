f3bc7c7629fa684a104fdfc7a3db2b3b11a57260
michbur <michalburdukiewicz@gmail.com>
2014-04-22 23:30:42 +0200

    added cleavage site search and implemented it into our procedure. The result is a distaster (AUC 0.41), I'm still investigating why.

--- functions.R
+++ functions.R
@@ -1,10 +1,11 @@
 # LIBRARIES ----------------------------
 
-library(dhmm)
+library(hsmm)
 library(seqinr)
 library(snowfall)
 library(kernlab)
 library(ROCR)
+library(caret)
 
 
 
@@ -246,7 +247,7 @@ start_model2 <- function(x, t1, t2, t3, t4){
 }
 
 #calculates concert of the single protein
-test_protein <- function(protein, signal, aa_list, t1, t2, t3, t4, memory = FALSE) {
+test_signal <- function(protein, signal, aa_list, t1, t2, t3, t4, memory = FALSE) {
   nhc <- find_nhc(protein, signal)
   
   if (memory) {
@@ -266,25 +267,26 @@ test_protein <- function(protein, signal, aa_list, t1, t2, t3, t4, memory = FALS
 }
 
 #seeks signal peptides in the list of proteins
-find_signal <- function(list_prots, ts, alpha = 2, paralell = FALSE) {
+#pot_sigs: potential signal cleavage site
+find_signal <- function(list_prots, ts, alpha = 2, model, paralell = FALSE) {
   
   #signal peptide must have at least 12 aa - assumption
-  pot_sigs <- seq(from = ifelse(round(ts[[1]] - alpha*ts[[2]], 0) > 12, round(ts[[1]] - alpha*ts[[2]], 0), 12), 
-                  to = round(ts[[1]] + alpha*ts[[2]], 0), by = 1)
+
   
   if (paralell) {
-    sfLapply(list_prots, function(protein)
-      recogn <- sapply(pot_sigs, function(cs) 
-        try(test_protein(protein, c(1, cs), aa5, ts[["t1"]], ts[["t2"]], ts[["t3"]], ts[["t4"]]), silent = TRUE)
-      )
+    sfLapply(list_prots, function(protein) {
+      pot_cl <- find_cleave(protein, model = model, ort_codes = ort_codes)
+      recogn <- sapply(pot_cl[pot_cl[, 1] > 0.5, 2], function(cs) 
+        try(test_signal(protein, c(1, cs), aa5, ts[["t1"]], ts[["t2"]], ts[["t3"]], ts[["t4"]]), silent = TRUE)
+      )}
     )
   } else {
-    lapply(list_prots, function(protein)
-      recogn <- sapply(pot_sigs, function(cs) 
-        try(test_protein(protein, c(1, cs), aa5, ts[["t1"]], ts[["t2"]], ts[["t3"]], ts[["t4"]]), silent = TRUE)
-      )
-    )       
-  }
+    lapply(list_prots, function(protein) {
+      pot_cl <- find_cleave(protein, model = model, ort_codes = ort_codes)
+      recogn <- sapply(pot_cl[pot_cl[, 1] > 0.5, 2], function(cs)  
+        try(test_signal(protein, c(1, cs), aa5, ts[["t1"]], ts[["t2"]], ts[["t3"]], ts[["t4"]]), silent = TRUE)
+      )}
+    )}     
 }
 
 #create subsets from data
@@ -331,3 +333,98 @@ read_signalp4 <- function(connection) {
   names(res) <- all_names
   res
 }
+
+
+# ortogonal coding ---------------------------------
+
+#code ort_codes
+encode_ort <- function(seq, codes)
+  as.vector(codes[seq, ])
+
+
+# test encoding
+# tmp <- matrix(sample(aa, 25*9, replace = TRUE), nrow = 25)
+# t(apply(tmp, 1, function(i)
+#   encode_ort(i, ort_codes)))
+
+
+# k-tuple encoding ---------------------------------
+
+create_features <- function(n, u) {
+  #creates a vector of all possible grams
+  #u - unigrams
+  #n - size of gram (i.e. 2-grams: "AA", "AC", ...)
+  grid_list <- lapply(1L:n, function(i) u)
+  apply(expand.grid(grid_list), 1, function(x)
+    paste(x, collapse = "."))
+}
+
+create_grams <- function(seq, n, d) {
+  #creates grams from given function
+  #n - size of gram (i.e. 2-grams: "AA", "AC", ...)
+  #d - distance between two consecutive aa
+  ind <- lapply(1:n, function(i) 
+    (1 + i - 1):(length(seq) - n + i))
+  
+  if(d > 0) {
+    ind[-1] <- lapply(ind[-1], function(i) i + d)
+    not_taken <- ind[[1]][(length(ind[[1]]) - d + 1):length(ind[[1]])]
+    ind <- lapply(ind, function(i) i[-not_taken])
+  }
+  
+  pair_matrix <- do.call(cbind, lapply(ind, function(i) seq[i]))
+  apply(pair_matrix, 1, function(x) 
+    paste(x, collapse="."))
+}
+
+count_grams <- function(seq, feature_list, n, d = 0, scale = FALSE) {
+  #feature list(list of possible n-grams) is outside, because count_grams is meant to
+  #be inside the loop
+  if (n > 1) {
+    grams <- create_grams(seq, n, d)
+  } else {
+    grams <- seq
+  }
+  res <- sapply(feature_list, function(i)
+    sum(i == grams))
+  if (scale)
+    res <- res/(length(seq) - n - 1)
+  res
+}
+
+get_kmer <- function(list_prot, k, d = 0) {
+  #get k-mer of aminoacids within distance d from cleaves place
+  if (k %% 2 != 0) {
+    k1 <- k2 <- floor(k/2)
+  } else {
+    k2 <- k/2
+    k1 <- k2 - 1
+  }
+  t(sapply(list_prot, function(ith_prot) 
+    ith_prot[(attr(ith_prot, "sig")[2] + d - k1):(attr(ith_prot, "sig")[2] + d + k2)]))
+}
+
+
+calc_unigrams <- function(seqs) {
+  #helper function for calculating unigrams
+  if (class(seqs) == "matrix") {
+    unigram <- apply(seqs, 1, function(i) data.frame(table(factor(i, levels = aa))))
+    len <- nrow(seqs)
+  } 
+  if (class(seqs) == "list") {
+    unigram <- lapply(seqs, function(i) data.frame(table(factor(i, levels = aa))))
+    len <- length(seqs)
+  }
+  unigram <- do.call(rbind, unigram)
+  agg_unigram <- aggregate(Freq ~ Var1, unigram, sum)
+  data.frame(AA = agg_unigram[[1]], freq = agg_unigram[[2]]/len)
+}
+
+find_cleave <- function(protein, model, ort_codes, start_search = 12, end_search = 36) {
+  coded <- apply(sapply(start_search:end_search, function(i) 
+    protein[i:(i + 8)]), 2, function(nonamer) {
+      encode_ort(nonamer, ort_codes)
+    })
+  matrix(c(predict(model, t(coded), type = "probabilities")[,2], start_search:end_search), ncol = 2)
+}
+  
--- procedures.R
+++ procedures.R
@@ -1,37 +1,128 @@
 # constants ----------------------------
 
-aa5 = list(positively.charged=c("K", "R", "H"),
-           hydrofobic=c("V","I","L","M","F","W","C"),
-           polar.uncharged=c("S", "T", "N", "Q"),
-           rest=c("D","E","A","P","Y","G"))
+aa <- a()[-1]
+
+aa1 = list('1' = c("G", "A", "P", "V", "L", "I", "M"), 
+           '2' = c("F", "W", "Y"), 
+           '3' = c("K", "R", "H"), 
+           '4' = c("D", "E"), 
+           '5' = c("S", "T", "C", "N", "Q"))
+
+aa2 = list('1' = c("G", "A", "P", "V", "L", "I", "M", "F"), 
+           '2' =c("K", "R", "H"), 
+           '3' =c("D", "E"), 
+           '4' =c("S", "T", "C", "N", "Q", "Y", "W"))
+
+aa3 = list('1' = c("G", "A", "P", "V", "L", "I", "M", "F", "W", "S", "T", "C", "N", "Q", "Y"), 
+           '2' = c("D", "E"), 
+           '3' = c("K", "R", "H"))
+
+aa4 = list('1' = c("G", "A", "V", "L", "I"), 
+           '2' = c("M", "C"), 
+           '3' = c("F", "W", "Y"), 
+           '4' = c("S", "T", "N", "Q"), 
+           '5' = c("K", "R", "H"), 
+           '6' = c("D", "E"), 
+           '7' ="P")
+
+aa5 = list('1' = c("K", "R", "H"),
+           '2' = c("V","I","L","M","F","W","C"),
+           '3' = c("S", "T", "N", "Q"),
+           '4' = c("D","E","A","P","Y","G"))
+
+aa6 <- list(`1` = c("A", "C", "Q", "E"), 
+            `2` = c("R", "H",  "K"), 
+            `3` = c("N", "D", "G", "P", "S", "T"), 
+            `4` = c("I", "L",  "M", "F", "W", "Y", "V"))
+
+#generate ort_codes
+ort_codes <- sapply(1:20, function(i) {
+  res <- rep(0, 20)
+  res[i] <- 1
+  res
+})
+
+rownames(ort_codes) <- aa
 
 # data ----------------
 
 euk <- read_uniprot("euk.txt", euk = TRUE)
 euk_not <- read.fasta("euk_not.fasta", seqtype = "AA")
 euk_not <- euk_not[-get_atyp(euk_not)]
+euk_not <- euk_not[-which(sapply(euk_not, length) > 4000)]
+euk_not <- euk_not[-which(sapply(euk_not, length) < 50)]
+
 
 pos <- split_prots(euk, replace = FALSE)
 neg <- split_prots(euk_not, replace = FALSE)
 neg[["valid"]] <- neg[["valid"]][1L:length(pos[["valid"]])]
 neg[["test"]] <- neg[["test"]][1L:length(pos[["test"]])]
 
-# testing algorithm ---------------------
+# searching cleave region ---------------------
+
+#########
+kmer = 9 
+prot_list = euk[pos[["train"]]]
+########
+
+cleaves <- get_kmer(prot_list, kmer)
+
+#parts of signal peptide without cleaving region
+n_cleaves1 <- get_kmer(prot_list[sapply(prot_list, function(ith_prot) 
+  attr(ith_prot, "sig")[2] > 16)], kmer, d = -round(kmer * 1.3, 0))
+
+#mature protein
+n_cleaves2 <- get_kmer(prot_list, kmer, d = round(kmer * 1.3, 0))
+n_cleaves <- rbind(n_cleaves1, n_cleaves2)
+colnames(cleaves) <- paste0("P", 1L:kmer)
+dat <- rbind(cbind(cleaves, et = rep("pos", nrow(cleaves))),
+             cbind(n_cleaves, et = rep("neg", nrow(n_cleaves))))
+
+edtrain <- cbind(t(apply(dat[, -ncol(dat)], 1, function(i)
+  encode_ort(i, ort_codes))), et = dat[, ncol(dat)])
+
+#training ----------------------
+sfInit(parallel=TRUE, cpus=6)
+ctrl <- trainControl(method = "repeatedcv",
+                     repeats = 3,
+                     classProbs = TRUE,
+                     summaryFunction = twoClassSummary,
+                     allowParallel = TRUE)
+
+svm_fit <- train(et ~ .,
+                 data = edtrain,
+                 method = "svmLinear",
+                 trace = FALSE,
+                 maxit = 50,
+                 trControl = ctrl,
+                 metric = "ROC")
+sfStop()
+
+
+
+# testing signal peptide ---------------------
 
 ts <- calc_t(euk[pos[["train"]]])
 
-sfInit( parallel=TRUE, cpus=4 )
+sfInit(parallel=TRUE, cpus=6)
 sfLibrary(depmixS4)
-sfLibrary(dhmm)
+sfLibrary(hsmm)
 
-sfExport("test_protein", "find_nhc", "degenerate", "start_model", "start_model2", 
+sfExport("encode_ort", "svm_fit", "ort_codes", "test_protein", "find_nhc", "find_cleave", "degenerate", "start_model", "start_model2", 
          "aa5", "ts", "euk", "euk_not", "pos", "neg")
-pos_valid <- find_signal(euk[pos[["valid"]]], ts, paralell = TRUE)
-neg_valid <- find_signal(euk_not[neg[["valid"]]], ts, paralell = TRUE)
-pos_test <- find_signal(euk[pos[["test"]]], ts, paralell = TRUE)
-neg_test <- find_signal(euk_not[neg[["test"]]], ts, paralell = TRUE)
+pos_valid <- find_signal(euk[pos[["valid"]]], ts, model = svm_fit$finalModel, paralell = TRUE)
+neg_valid <- find_signal(euk_not[neg[["valid"]]], ts, model = svm_fit$finalModel, paralell = TRUE)
+pos_test <- find_signal(euk[pos[["test"]]], ts, model = svm_fit$finalModel, paralell = TRUE)
+neg_test <- find_signal(euk_not[neg[["test"]]], ts, model = svm_fit$finalModel, paralell = TRUE)
 
 sfStop()
+
+pos_valid <- find_signal(euk[pos[["valid"]]], ts, model = svm_fit$finalModel, paralell = FALSE)
+neg_valid <- find_signal(euk_not[neg[["valid"]]][5:6], ts, model = svm_fit$finalModel, paralell = FALSE)
+pos_test <- find_signal(euk[pos[["test"]]], ts, model = svm_fit$finalModel, paralell = FALSE)
+neg_test <- find_signal(euk_not[neg[["test"]]], ts, model = svm_fit$finalModel, paralell = FALSE)
+
+
 
 valid <- rbind(list_to_df(pos_valid, "pos"), list_to_df(neg_valid, "neg"))
 test <- rbind(list_to_df(pos_test, "pos"), list_to_df(neg_test, "neg"))
